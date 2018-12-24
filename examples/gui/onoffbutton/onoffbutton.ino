8a81392e0da78be69d70ee4f8eafb1c45065ae2d
mharizanov <mharizanov@gmail.com>
2018-12-24 14:17:57 +0200

    Add files via upload

---
+++ examples/gui/onoffbutton/onoffbutton.ino
@@ -0,0 +1,86 @@
+//This example implements a simple sliding On/Off button. The example
+// demonstrates drawing and touch operations.
+//
+//Thanks to Adafruit forums member Asteroid for the original sketch!
+//
+#include <Adafruit_GFX.h>
+#include "TFT32_Adafruit_ILI9341.h"
+
+#include "SPI.h"
+
+#define cs   15  // goes to TFT CS
+#define dc   2  // goes to TFT DC
+#define rst  27  // goes to TFT RESET
+#define TFT_BKLT 4
+
+TFT32_Adafruit_ILI9341 tft = TFT32_Adafruit_ILI9341(cs, dc, rst);
+
+boolean RecordOn = false;
+
+#define FRAME_X 210
+#define FRAME_Y 180
+#define FRAME_W 100
+#define FRAME_H 50
+
+#define REDBUTTON_X FRAME_X
+#define REDBUTTON_Y FRAME_Y
+#define REDBUTTON_W (FRAME_W/2)
+#define REDBUTTON_H FRAME_H
+
+#define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+#define GREENBUTTON_Y FRAME_Y
+#define GREENBUTTON_W (FRAME_W/2)
+#define GREENBUTTON_H FRAME_H
+
+void drawFrame()
+{
+  tft.drawRect(FRAME_X, FRAME_Y, FRAME_W, FRAME_H, ILI9341_BLACK);
+}
+
+void redBtn()
+{ 
+  tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_RED);
+  tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_BLUE);
+  drawFrame();
+  tft.setCursor(GREENBUTTON_X + 6 , GREENBUTTON_Y + (GREENBUTTON_H/2));
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.println("ON");
+  RecordOn = false;
+}
+
+void greenBtn()
+{
+  tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_GREEN);
+  tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_BLUE);
+  drawFrame();
+  tft.setCursor(REDBUTTON_X + 6 , REDBUTTON_Y + (REDBUTTON_H/2));
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.println("OFF");
+  RecordOn = true;
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+
+  pinMode(TFT_BKLT, OUTPUT);
+  digitalWrite(TFT_BKLT, LOW);
+  
+  tft.begin();
+  tft.fillScreen(ILI9341_BLUE);
+  tft.setRotation(1); 
+}
+
+void loop()
+{
+ redBtn();
+ delay(500);
+ greenBtn();
+ delay(500);
+}
+
+
+
